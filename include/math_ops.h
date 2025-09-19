dfb598bf9c076eb1da3f74db5a64cb0af00ca33f
James Brady <jbrady@mtech.edu>
2025-09-18 21:49:42 -0600

    Consolidated math functions

---
+++ include/math_ops.h
@@ -0,0 +1,10 @@
+#ifndef MATH_OPS_H
+#define MATH_OPS_H
+
+float multiply(float x, float y)
+float add(float x, float y)
+float square(float x)
+float cube(float x)
+float pow(float base, int exponent)
+
+#endif
